256c0115aba3d28ca16f011488c8d337feeb68cc
gabriel <spontoni33@gmail.com>
2024-02-07 13:51:21 -0400

    feat: add student repo and questions entities

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/questions/entities/AlternativesEntity.java
@@ -0,0 +1,35 @@
+package com.rocketseat.certification_nlw.modules.questions.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "alternatives")
+
+public class AlternativesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 50)
+    private String technology;
+
+    @Column()
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "question_id")
+    private List<AlternativesEntity> alternatives;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
--- src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,13 +1,19 @@
 package com.rocketseat.certification_nlw.modules.students.useCases;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyHasCertificationDTO dto) {
-        return dto.getEmail().equals("test@gmail.com") && dto.getTechnology().equals("java");
+        var result = this.certificationStudentRepository.findByEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        return !result.isEmpty();
     }
 }
